96e33703e1422e8879f3ac105b7ded32cf9ef88d
Simon Brown <si@sjbrown.co.uk>
2020-12-06 09:48:51 +0000

    Part 2

--- src/day04.rs
+++ src/day04.rs
@@ -45,6 +45,53 @@ impl<'a> Passport<'a> {
             && self.ecl.is_some()
             && self.pid.is_some()
     }
+
+    fn is_valid_alt(&self) -> bool {
+        self.is_valid()
+            && self
+                .byr
+                .and_then(|s| s.parse::<usize>().ok())
+                .map(|n| matches!(n, 1920..=2002))
+                .unwrap_or(false)
+            && self
+                .iyr
+                .and_then(|s| s.parse::<usize>().ok())
+                .map(|n| matches!(n, 2010..=2020))
+                .unwrap_or(false)
+            && self
+                .eyr
+                .and_then(|s| s.parse::<usize>().ok())
+                .map(|n| matches!(n, 2020..=2030))
+                .unwrap_or(false)
+            && self
+                .hgt
+                .map(|s| {
+                    if let Some(h) = s.strip_suffix("cm") {
+                        matches!(h.parse::<usize>().unwrap(), 150..=193)
+                    } else if let Some(h) = s.strip_suffix("in") {
+                        matches!(h.parse::<usize>().unwrap(), 59..=76)
+                    } else {
+                        false
+                    }
+                })
+                .unwrap_or(false)
+            && self
+                .hcl
+                .map(|s| {
+                    s.len() == 7
+                        && s.starts_with("#")
+                        && s[1..].chars().all(|c| matches!(c, '0'..='9' | 'a'..='f'))
+                })
+                .unwrap_or(false)
+            && self
+                .ecl
+                .map(|s| matches!(s, "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth"))
+                .unwrap_or(false)
+            && self
+                .pid
+                .map(|s| s.len() == 9 && s.chars().all(|c| matches!(c, '0'..='9')))
+                .unwrap_or(false)
+    }
 }
 
 fn is_ident(c: char) -> bool {
@@ -77,4 +124,6 @@ pub fn run() {
     .1;
     let valid_count = passports.iter().filter(|p| p.is_valid()).count();
     println!("day04: valid count is {}", valid_count);
+    let alt_valid_count = passports.iter().filter(|p| p.is_valid_alt()).count();
+    println!("day04: alt valid count is {}", alt_valid_count);
 }
