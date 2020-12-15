fbc82b880c2b804e793473aafc735c052869238a
Simon Brown <si@sjbrown.co.uk>
2020-12-15 20:38:53 +0000

    Day 15 part 1

---
+++ src/day15.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+pub fn run() {
+    let mut seq = vec![1, 17, 0, 10, 18, 11, 6];
+    let mut index_by_val = HashMap::new();
+    let mut replaced_index = None;
+    for (index, val) in seq.iter().cloned().enumerate() {
+        replaced_index = index_by_val.insert(val, index);
+    }
+
+    while seq.len() < 2020 {
+        let val = if let Some(first_index) = replaced_index {
+            let second_index = seq.len() - 1;
+            second_index - first_index
+        } else {
+            0
+        };
+        let index = seq.len();
+        seq.push(val);
+        replaced_index = index_by_val.insert(val, index);
+    }
+
+    println!("day15: 2020th is {}", seq.last().unwrap());
+}
