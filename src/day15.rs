af775e39cb5a855dd24b483458344e8b98e2f4e1
Simon Brown <si@sjbrown.co.uk>
2020-12-15 20:40:26 +0000

    Part 2 (probably the slow way)

--- src/day15.rs
+++ src/day15.rs
@@ -19,6 +19,18 @@ pub fn run() {
         seq.push(val);
         replaced_index = index_by_val.insert(val, index);
     }
-
     println!("day15: 2020th is {}", seq.last().unwrap());
+
+    while seq.len() < 30000000 {
+        let val = if let Some(first_index) = replaced_index {
+            let second_index = seq.len() - 1;
+            second_index - first_index
+        } else {
+            0
+        };
+        let index = seq.len();
+        seq.push(val);
+        replaced_index = index_by_val.insert(val, index);
+    }
+    println!("day15: 30000000th is {}", seq.last().unwrap());
 }
