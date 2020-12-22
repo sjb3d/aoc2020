18ab5868428c3b70f59dd0fa571e1bb9f10d8960
Simon Brown <si@sjbrown.co.uk>
2020-12-22 10:02:25 +0000

    Part 2

--- src/day22.rs
+++ src/day22.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-#[derive(Clone)]
+#[derive(Clone, Eq, PartialEq)]
 struct Player {
     deck: VecDeque<usize>,
 }
@@ -11,7 +11,12 @@ impl Player {
     }
 
     fn score(&self) -> usize {
-        self.deck.iter().rev().enumerate().map(|(i, v)| (i + 1) * v).sum()
+        self.deck
+            .iter()
+            .rev()
+            .enumerate()
+            .map(|(i, v)| (i + 1) * v)
+            .sum()
     }
 }
 
@@ -24,6 +29,58 @@ fn play(p1: &mut Player, p2: &mut Player) {
     } else {
         p2.deck.push_back(c2);
         p2.deck.push_back(c1);
+    }
+}
+
+enum PlayerIndex {
+    P1,
+    P2,
+}
+
+fn play_recursive_game(p1: &mut Player, p2: &mut Player) -> PlayerIndex {
+    let mut history = Vec::new();
+    loop {
+        for (old1, old2) in history.iter() {
+            if old1 == p1 && old2 == p2 {
+                return PlayerIndex::P1;
+            }
+        }
+        history.push((p1.clone(), p2.clone()));
+
+        let c1 = p1.deck.pop_front().unwrap();
+        let c2 = p2.deck.pop_front().unwrap();
+
+        let winner = if p1.deck.len() >= c1 && p2.deck.len() >= c2 {
+            let mut q1 = Player {
+                deck: p1.deck.iter().cloned().take(c1).collect(),
+            };
+            let mut q2 = Player {
+                deck: p2.deck.iter().cloned().take(c2).collect(),
+            };
+            play_recursive_game(&mut q1, &mut q2)
+        } else {
+            if c1 > c2 {
+                PlayerIndex::P1
+            } else {
+                PlayerIndex::P2
+            }
+        };
+        match winner {
+            PlayerIndex::P1 => {
+                p1.deck.push_back(c1);
+                p1.deck.push_back(c2);
+                if p2.deck.is_empty() {
+                    return PlayerIndex::P1;
+                }
+            }
+            PlayerIndex::P2 => {
+                p2.deck.push_back(c2);
+                p2.deck.push_back(c1);
+                if p1.deck.is_empty() {
+                    return PlayerIndex::P2;
+                }
+            }
+        }
     }
 }
 
@@ -58,4 +115,15 @@ pub fn run() {
         }
     };
     println!("day22: winning score is {}", score);
+
+    let score = {
+        let mut p1 = player1.clone();
+        let mut p2 = player2.clone();
+        let winner = play_recursive_game(&mut p1, &mut p2);
+        match winner {
+            PlayerIndex::P1 => p1.score(),
+            PlayerIndex::P2 => p2.score(),
+        }
+    };
+    println!("day22: winning recursive score is {}", score);
 }
